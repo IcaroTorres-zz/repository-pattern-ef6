70bdaca9a4571747f5b948e8d1250c06f6fd96c3
Ícaro "Stuart" da Silva Torres <icaro.stuart@gmail.com>
2019-09-12 18:20:54 -0300

    unit of work action filter

---
+++ UnitOfWorkActionFilter.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Synapse.Repository
+{
+    public class UnitOfWorkActionFilter : ActionFilterAttribute
+    {
+        public IUnitOfWork<DbContext> UnitOfWork { get; private set; }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            UnitOfWork = actionContext.Request.GetDependencyScope().GetService(typeof(IUnitOfWork<DbContext>)) as IUnitOfWork<DbContext>;
+            UnitOfWork.BeginTransaction();
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            UnitOfWork = actionExecutedContext.Request.GetDependencyScope().GetService(typeof(IUnitOfWork<DbContext>)) as IUnitOfWork<DbContext>;
+            if (actionExecutedContext.Exception == null)
+            {
+                // commit if no exceptions
+                UnitOfWork.Commit();
+            }
+            else
+            {
+                // rollback if exception
+                UnitOfWork.Rollback();
+            }
+        }
+    }
+}
